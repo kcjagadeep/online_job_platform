a7226327398774153eebeada73e1c7ea86ebdcb1
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2025-07-24 20:20:01 +0530

    complete project for render deployemnt

--- job_platform/urls.py
+++ job_platform/urls.py
@@ -26,3 +26,9 @@ urlpatterns = [
     path('api/', include('jobapp.api.urls')),   # 👈 separate for API
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
